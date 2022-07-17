fc453443dda0fe9e13c88ba04f6976c669b47ed2
ScrPzz <alessandrotogni1985@gmail.com>
2022-07-18 00:19:23 +0200

    upd

--- file_utils.py
+++ file_utils.py
@@ -4,10 +4,7 @@ import os
 import cv2
 import numpy as np
 
-import imgproc
 
-
-# borrowed from https://github.com/lengstrom/fast-style-transfer/blob/master/src/utils.py
 def get_files(img_dir):
     imgs, masks, xmls = list_files(img_dir)
     return imgs, masks, xmls
